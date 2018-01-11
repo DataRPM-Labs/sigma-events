9f2bd41d7613b9195069290185004f4ec712406b
Vishal <vishal@datarpm.com>
2018-01-11 12:59:22 +0530

    build script changes

--- sbuild/build.sh
+++ sbuild/build.sh
@@ -19,7 +19,7 @@ scriptHome="`cd "$scriptHome"; pwd`"
 cd $scriptHome/../;
 projectHome=$scriptHome/../
 
-unset $SIGMA_EVENTENGINE_HOME
-unset $SIGMA_EVENTENGINE_CONF_DIR
+unset SIGMA_EVENTENGINE_HOME
+unset SIGMA_EVENTENGINE_CONF_DIR
 
 mvn clean install verify gpg:sign
